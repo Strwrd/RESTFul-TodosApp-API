92a2960bf08f157063631d64e35f02684c15cd0d
strwrd <wiguna.bagussatria@gmail.com>
2017-11-10 21:20:57 +0700

    Add comment for documentation

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
+//Init env
 const env = process.env.NODE_ENV || 'development';
 
+//Select env based on run command
 if (env === 'development'){
 	process.env.PORT = 3000;
 	process.env.MONGODB_URI = 'mongodb://127.0.0.1/TodosApp';
